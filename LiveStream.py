47d3c01515ef399d70a36aa90d3cd2a9ff52185d
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-10-18 21:16:51 +0200

    Update LiveStream.py

--- LiveStream.py
+++ LiveStream.py
@@ -1,1 +1,31 @@
 
+import cv2
+from flask import Flask, Response, render_template
+
+app = Flask(__name__)
+
+cap = cv2.VideoCapture(0)  # Zachytávanie obrazovky. Číslo 0 označuje hlavnú kameru.
+
+def generate_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                continue
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
