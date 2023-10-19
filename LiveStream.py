d4ead802b37a85697736861789593a5528653e49
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-10-19 18:06:24 +0200

    Update LiveStream.py

--- LiveStream.py
+++ LiveStream.py
@@ -1,31 +1,38 @@
 import cv2
 import pyautogui
-from flask import Flask, Response, render_template_string
-from datetime import datetime
+import datetime
+import os
+from flask import Flask, Response, send_from_directory
 
 app = Flask(__name__)
+screenshot_directory = "screenshots"
 
-connected_users = []
+if not os.path.exists(screenshot_directory):
+    os.makedirs(screenshot_directory)
 
-def generate_frames():
-    while True:
-        screenshot = pyautogui.screenshot()
-        frame = cv2.cvtColor(
-            cv2.resize(
-                cv2.cvtColor(
-                    frame, 
-                    cv2.COLOR_BGR2RGB
-                ), 
-                (800, 600)
-            ), 
-            cv2.COLOR_BGR2RGB
-        )
-        ret, buffer = cv2.imencode('.jpg', frame)
-        if not ret:
-            continue
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+@app.route('/current_screenshot')
+def current_screenshot():
+    capture_and_save_screenshot()  # Zachyti a uloží aktuálny screenshot
+    latest_screenshot = get_latest_screenshot()
+    if latest_screenshot:
+        return send_from_directory(screenshot_directory, latest_screenshot)
+    else:
+        return "No screenshots available."
+
+def get_latest_screenshot():
+    screenshot_files = os.listdir(screenshot_directory)
+    screenshot_files.sort()
+    if screenshot_files:
+        latest_screenshot = screenshot_files[-1]
+        return latest_screenshot
+    else:
+        return None
+
+def capture_and_save_screenshot():
+    screenshot = pyautogui.screenshot()
+    current_time = datetime.datetime.now()
+    screenshot_filename = os.path.join(screenshot_directory, f"screenshot_{current_time.strftime('%Y%m%d%H%M%S')}.png")
+    screenshot.save(screenshot_filename)
 
 @app.route('/')
 def index():
@@ -33,31 +40,33 @@ def index():
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Live Stream</title>
+        <title>Live Screen</title>
+        <style>
+            body {
+                background-color: red;
+                text-align: center;
+            }
+            #screenshot {
+                border: 6px solid black;
+                border-radius: 2%;  /* Zaoblenie rohov */
+                max-width: 100%;
+            }
+        </style>
     </head>
     <body>
-        <img src="{{ url_for('video') }}" style="width: 100%; height: auto;">
+        <button onclick="loadScreenshot()">Load New Screenshot</button>
+        <br>
+        <img id="screenshot" src="/current_screenshot" alt="Current Screenshot" style="width: 1200px; height: 800px; display: block; margin: 0 auto; margin-top: 200px;">
+        <script>
+            function loadScreenshot() {
+                var screenshotElement = document.getElementById('screenshot');
+                screenshotElement.src = "/current_screenshot?" + new Date().getTime();
+            }
+        </script>
     </body>
     </html>
     """
-    return render_template_string(html)
-
-@app.route('/video')
-def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/connect')
-def connect():
-    client_address = request.remote_addr
-    connected_users.append(client_address)
-    log_connected_users(client_address)
-    return "Connected to the stream!"
-
-def log_connected_users(client_address):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open('ConnectedUsers.txt', 'a') as file:
-        file.write(f"{current_time}\nConnected User: {client_address}\n\n")
-    print(f"{current_time}\nConnected User: {client_address}")
+    return html
 
 if __name__ == '__main__':
     app.run(host='192.168.56.1', port=5000, debug=True)
